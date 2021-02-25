7e813c5a63cac1cd9ba5d2224037a2a692b42936
mihajlo33-dev <mdrljaca@rocektmail.com>
2021-02-25 08:59:26 +0100

    commit 12

--- flask-angular-boilerplate/runserver.py
+++
@@ -1,7 +0,0 @@
-from backend.factory import create_app
-from backend.config import Config
-import os
-
-if __name__ == '__main__':
-        app = create_app(Config)
-        app.run(port=5000, host="0.0.0.0", use_reloader=True)
